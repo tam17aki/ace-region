e66be2fbdca7d546466b067ad291b86ffae1500b
Akira Tamamori <tamamori5917@gmail.com>
2015-01-15 11:53:49 +0900

    Add minor mode of ace-region-mode.

--- ace-region.el
+++ ace-region.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/tam17aki/ace-region
 ;; Version: 0.01
 ;; Created: Jan 11 2015
-;; Package-Requires: ((ace-jump-mode "2.0") (region-bindings-mode "1.4") (emacs "24"))
+;; Package-Requires: ((ace-jump-mode "2.0") (region-bindings-mode "1.4"))
 
 ;; This program is free software; you can redistribute it and/or modify it under
 ;; the terms of the GNU General Public License as published by the Free Software
@@ -24,29 +24,63 @@
 ;;; Commentary:
 
 ;;; Installation:
+;;
+;; To use this package, add following code to your init file.
+;;
 ;; (require 'ace-region)
+;; (global-ace-region-mode +1)
 
 ;;; Code:
 (require 'ace-jump-mode)
 (require 'region-bindings-mode)
 
-(define-key region-bindings-mode-map (kbd "C-j")
-  #'(lambda ()
-      (interactive)
-      (exchange-point-and-mark)
-      (let ((ace-jump-mode-scope 'window))
-        (call-interactively 'ace-jump-char-mode))))
+(defgroup ace-region nil
+  "Group of ace-region."
+  :group 'convenience)
+
+(defcustom ace-region-lighter " AceR"
+  "Lighter of ace-region-mode."
+  :type 'string
+  :group 'ace-region)
+
+(defcustom ace-region-jump-key "C-j" nil
+  :type 'string
+  :group 'ace-region)
 
 (defun ace-region--forward-backward-char ()
   (when (region-active-p)
     (cond ((< (mark) (point))
            (forward-char))
-          ((> (mark) (point))
+          (t
            (backward-char)))))
 
-(add-hook 'ace-jump-mode-end-hook #'ace-region--forward-backward-char)
+(defun ace-region--jump-function ()
+  (interactive)
+  (exchange-point-and-mark)
+  (let ((ace-jump-mode-scope 'window))
+    (call-interactively 'ace-jump-char-mode)))
 
-;; (remove-hook 'ace-jump-mode-end-hook #'ace-region--forward-backward-char)
+;;;###autoload
+(define-minor-mode ace-region-mode
+  "Minor-mode which provides some utilities for region expansion."
+  :group      'ace-region
+  :init-value nil
+  :global     nil
+  :lighter    ace-region-mode-lighter
+  (cond (ace-region-mode
+         (add-hook 'ace-jump-mode-end-hook #'ace-region--forward-backward-char nil t)
+         (define-key region-bindings-mode-map
+           (kbd ace-region-jump-key) #'ace-region--jump-function))
+        (t
+         (remove-hook 'ace-jump-mode-end-hook #'ace-region--forward-backward-char t)
+         (define-key region-bindings-mode-map (kbd ace-region-jump-key) nil))))
+
+(defun ace-region--turn-on ()
+  (unless (minibufferp)
+    (ace-region-mode +1)))
+
+(define-globalized-minor-mode global-ace-region-mode ace-region-mode ace-region--turn-on
+  :group 'ace-region)
 
 (provide 'ace-region)
 ;;; ace-region.el ends here
